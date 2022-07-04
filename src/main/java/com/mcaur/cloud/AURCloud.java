aff6d9aef4e9477e23db268fd58b6abe95c0b288
Kawa1Ares <kawaiiares@tom.com>
2022-07-04 16:03:48 +0800

    ;w; I won't spark

--- src/main/java/com/mcaur/cloud/AURCloud.java
+++ src/main/java/com/mcaur/cloud/AURCloud.java
@@ -1,4 +1,9 @@
 package com.mcaur.cloud;
 
+import static spark.Spark.get;
+
 public class AURCloud {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
 }
